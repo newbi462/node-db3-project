94c76651ab6fe48db948dbb8eeaaa79707dd61ea
newbi462 <isthisemailfree@gmail.com>
2020-01-22 17:44:42 -0500

    function findSteps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,15 +3,41 @@ const db = require('../data/db.js');
 module.exports = {
   find,
   findById,
+  findSteps,
 }
 
 function find() {
   //https://github.com/knex/knex/issues/360
   // SELECT name FROM sqlite_master WHERE type='table';
-  return db.select("name").from("sqlite_master").where({type: "table"});
+  return db.select("*").from("schemes");
 }
 
 
-function findById(id) {
-
+function findById(idPassed) {
+  return db("schemes").where({id: idPassed})
 }
+
+function findSteps(idPassed) {
+  return db("steps")
+    .join("schemes", "steps.scheme_id", "schemes.id" )
+    .select("steps.id", "schemes.scheme_name", "steps.step_number", "steps.instructions" )
+    .where({scheme_id: idPassed})
+    .orderBy('step_number', 'asc')
+}
+
+/*
+
+return db("posts as p")
+    .join("users as u", "p.user_id", "u.id")
+    .where("p.id", id)
+    .select("p.id as postId", "p.contents", "u.username as postedBy")
+    .first();
+
+
+{ id: 17,
+scheme_name: 'Find the Holy Grail',
+
+step_number: 1,
+
+instructions: 'quest'}
+*/
