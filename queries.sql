a0c2c154120a60a798523bfc50167c9b6739cc95
newbi462 <isthisemailfree@gmail.com>
2020-01-22 14:59:59 -0500

    -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.

--- queries.sql
+++ queries.sql
@@ -5,7 +5,9 @@ SELECT product.ProductName, category.Categoryname FROM product
 JOIN category ON product.CategoryId = category.id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-
+SELECT [order].Id, shipper.CompanyName FROM [order]
+JOIN shipper ON [order].ShipVia = shipper.id
+WHERE [order].OrderDate < '2012-08-09';
 
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
