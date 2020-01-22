69f38695f97983ecd8247b29148a24a46d2ad1eb
newbi462 <isthisemailfree@gmail.com>
2020-01-22 15:13:04 -0500

    -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.

--- queries.sql
+++ queries.sql
@@ -9,7 +9,9 @@ SELECT [order].Id, shipper.CompanyName FROM [order]
 JOIN shipper ON [order].ShipVia = shipper.id
 WHERE [order].OrderDate < '2012-08-09';
 
-
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+SELECT [order].ShipName, OrderDetail.Quantity FROM OrderDetail
+JOIN [order] ON OrderDetail.OrderID = [order].id
+WHERE [order].id = 10251;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
